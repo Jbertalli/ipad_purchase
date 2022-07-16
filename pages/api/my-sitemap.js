26f453f909c927c4bcad9c8d30e299810a8c43ba
Jbertalli <jjbertalli@berkeley.edu>
2022-07-16 02:18:14 -0700

    SEO

---
+++ pages/api/my-sitemap.js
@@ -0,0 +1,29 @@
+const { SitemapStream, streamToPromise } = require( 'sitemap' );
+const { Readable } = require( 'stream' );
+
+export default async (req, res) => {
+    // An array with your links
+    const links = [
+        { url: '/pages/buyipad', changefreq: 'daily', priority: 0.3  }, 
+        { url: '/pages/ipadair', changefreq: 'daily', priority: 0.3  },
+        { url: '/pages/colorSelection/ipadhome', changefreq: 'daily', priority: 0.3  },
+        { url: '/pages/colorSelection/grayipad', changefreq: 'daily', priority: 0.3  },
+        { url: '/pages/colorSelection/pinkipad', changefreq: 'daily', priority: 0.3  },
+        { url: '/pages/colorSelection/purpleipad', changefreq: 'daily', priority: 0.3  },
+        { url: '/pages/colorSelection/blueipad', changefreq: 'daily', priority: 0.3  },
+        { url: '/pages/colorSelection/starlightipad', changefreq: 'daily', priority: 0.3  }
+    ];
+
+    // Create a stream to write to
+    const stream = new SitemapStream( { hostname: `https://${req.headers.host}` } );
+
+    res.writeHead(200, {
+        "Content-Type": "application/xml",
+    });
+
+    const xmlString = await streamToPromise(
+        Readable.from(links).pipe(stream)
+    ).then((data) => data.toString());
+
+    res.end(xmlString)
+}
