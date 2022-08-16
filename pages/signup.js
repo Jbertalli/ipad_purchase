d42cb6a285ae57feffe1eb83e3033aeb6b206f83
Jbertalli <jjbertalli@berkeley.edu>
2022-08-16 00:25:17 -0700

    payment card

--- pages/signup.js
+++ pages/signup.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-import { Divider, Container, Button, Icon, Modal, Form, Message } from 'semantic-ui-react';
+import Image from 'next/image';
+import { Divider, Container, Button, Icon, Modal, Form, Message, Card, Icons } from 'semantic-ui-react';
 import styles from '../styles/ipad.module.css';
 import FocusLock from 'react-focus-lock';
 import catchErrors from '../utils/catchErrors';
@@ -54,18 +55,43 @@ export default function Signup() {
             </Head>
             { user ? (
             <>
-                <Button onClick={(() => {
-                    checkout({
-                        lineItems: [
-                            {
-                                price: "price_1LXANZAd33GNgngt1Ni0igfE",
-                                quantity: 1
-                            }
-                        ]
-                    })
-                })}>
-                    iPad 64GB + WiFi
-                </Button>
+                <Card style={{ padding: '15px', borderRadius: '10px', fontFamily: 'SF Pro' }}>
+                    <Image src="/images/ipad 2.png" alt="ipad" width={300} height={195} />
+                    <b style={{ margin: '10px 0px 10px 0px' }}>iPad 64GB + WiFi</b>
+                    <p style={{ lineHeight: '15px' }}>
+                        The new iPad Air has an all-screen design, 10.9″ display, M1 chip, Center Stage, works with Apple Pencil and Magic Keyboard, and comes in five colors.
+                    </p>
+                    <b>
+                        $654.66
+                    </b>
+                    <b style={{ fontWeight: '500', marginBottom: '10px' }}>
+                        <Icon name="check" /> In stock
+                    </b>
+                    <Button
+                        onClick={(() => {
+                            checkout({
+                                lineItems: [
+                                    {
+                                        price: "price_1LXANZAd33GNgngt1Ni0igfE",
+                                        quantity: 1
+                                    }
+                                ]
+                            })
+                        })}
+                        style={{ 
+                            borderRadius: '50px 50px 50px 50px',
+                            background: 'black', 
+                            color: 'white', 
+                            fontWeight: '100', 
+                            fontSize: '17px',
+                            padding: '10px',
+                        }}
+                    >
+                        <span>
+                            Buy with <Icon name="apple" />Pay
+                        </span>
+                    </Button>
+                </Card>
                 <Button onClick={(() => {
                     checkout({
                         lineItems: [
