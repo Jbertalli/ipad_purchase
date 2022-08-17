a35911d507d5cc8039715e0b13a857010e71b6f3
Jbertalli <jjbertalli@berkeley.edu>
2022-08-17 01:16:12 -0700

    64CellAppleCare modal

--- components/paymentCards.js/64WifiAppleCare.js
+++ components/paymentCards.js/64WifiAppleCare.js
@@ -10,47 +10,47 @@ export default function SixWifiAppleCare() {
                     <Image src="/images/ipad 2.png" alt="ipad" width={700} height={460} />
                 </div>
                 <div style={{ transform: 'translateY(70px)', position: 'relative', zIndex: '10' }}>
-                <div style={{ fontSize: '40px', fontWeight: '500', paddingLeft: '56px' }}>iPad Air WiFi 64<span style={{ fontSize: '25px' }}>GB</span> with AppleCare+</div>
-                <Divider />
-                <p style={{ fontSize: '17px', padding: '0px 55px 0px 55px' }}>
-                    The new iPad Air has an all-screen design, 10.9″ display, M1 chip, Center Stage, works with Apple Pencil and Magic Keyboard, and comes in five colors.
-                </p>
-                <div>
-                    <b style={{ fontSize: '24px', padding: '0px 55px 58px 55px' }}>
-                        $740.47
-                    </b>
-                </div>
-                <div style={{ padding: '5px 55px 0px 51px' }}>
-                    <b style={{ fontSize: '17px', fontWeight: '500' }}>
-                        <Icon name="check" size="large" /> In stock
-                    </b>
-                </div>
-                <center>
-                <Button
-                    onClick={(() => {
-                        checkout({
-                            lineItems: [
-                                {
-                                    price: "price_1LXAOeAd33GNgngtTqx8R1sJ",
-                                    quantity: 1
-                                }
-                            ]
-                        })
-                    })}
-                    style={{ 
-                        borderRadius: '50px 50px 50px 50px',
-                        background: 'black', 
-                        color: 'white', 
-                        fontWeight: '100', 
-                        fontSize: '24px',
-                        padding: '15px 100px 15px 100px'
-                    }}
-                >
-                    <span>
-                        Buy with <Icon name="apple" />Pay
-                    </span>
-                </Button>
-                </center>
+                    <div style={{ fontSize: '40px', fontWeight: '500', paddingLeft: '56px' }}>iPad Air WiFi 64<span style={{ fontSize: '25px' }}>GB</span> with AppleCare+</div>
+                    <Divider />
+                    <p style={{ fontSize: '17px', padding: '0px 55px 0px 55px' }}>
+                        The new iPad Air has an all-screen design, 10.9″ display, M1 chip, Center Stage, works with Apple Pencil and Magic Keyboard, and comes in five colors.
+                    </p>
+                    <div>
+                        <b style={{ fontSize: '24px', padding: '0px 55px 58px 55px' }}>
+                            $740.47
+                        </b>
+                    </div>
+                    <div style={{ padding: '5px 55px 0px 51px' }}>
+                        <b style={{ fontSize: '17px', fontWeight: '500' }}>
+                            <Icon name="check" size="large" /> In stock
+                        </b>
+                    </div>
+                    <center>
+                        <Button
+                            onClick={(() => {
+                                checkout({
+                                    lineItems: [
+                                        {
+                                            price: "price_1LXAOeAd33GNgngtTqx8R1sJ",
+                                            quantity: 1
+                                        }
+                                    ]
+                                })
+                            })}
+                            style={{ 
+                                borderRadius: '50px 50px 50px 50px',
+                                background: 'black', 
+                                color: 'white', 
+                                fontWeight: '100', 
+                                fontSize: '24px',
+                                padding: '15px 100px 15px 100px'
+                            }}
+                        >
+                            <span>
+                                Buy with <Icon name="apple" />Pay
+                            </span>
+                        </Button>
+                    </center>
                 </div>
             </div>
         </>
