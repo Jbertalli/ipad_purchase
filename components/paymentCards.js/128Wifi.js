d66ade9972a80dbf79d69381239235e555b2d3f2
Jbertalli <jjbertalli@berkeley.edu>
2022-08-19 17:27:29 -0700

    payment cart update 7

--- components/paymentCards.js/128Wifi.js
+++ components/paymentCards.js/128Wifi.js
@@ -1,6 +1,7 @@
 import { checkout } from '../../checkout';
 import { Icon, Button, Divider } from 'semantic-ui-react';
 import Image from 'next/image';
+import Check from '../check';
 
 export default function OneWifi() {
     return (
@@ -9,21 +10,39 @@ export default function OneWifi() {
                 <div style={{ transform: 'translate(45px)' }}>
                     <Image src="/images/ipad 2.png" alt="ipad" width={700} height={460} />
                 </div>
-                <div style={{ transform: 'translateY(70px)', position: 'relative', zIndex: '10' }}>
-                    <div style={{ fontSize: '40px', fontWeight: '500', paddingLeft: '56px' }}>iPad Air WiFi 128<span style={{ fontSize: '25px' }}>GB</span></div>
+                <div style={{ transform: 'translateY(70px)', position: 'relative', zIndex: '10', margin: '0px 90px 0px 90px' }}>
+                    <div style={{ fontSize: '40px', paddingLeft: '56px' }}>iPad Air WiFi 128<span style={{ fontSize: '25px' }}>GB</span></div>
                     <Divider />
-                    <p style={{ fontSize: '17px', padding: '0px 55px 20px 55px' }}>
+                    <p style={{ fontSize: '17px', padding: '0px 55px 0px 55px' }}>
                         The new iPad Air has an all-screen design, 10.9″ display, M1 chip, Center Stage, works with Apple Pencil and Magic Keyboard, and comes in five colors.
                     </p>
-                    <div>
-                        <b style={{ fontSize: '24px', padding: '0px 55px 58px 55px' }}>
-                            $817.60
-                        </b>
+                    <div style={{ padding: '5px 55px 20px 51px', transform: 'translateY(-10px)' }}>
+                        <div style={{ fontSize: '17px', fontWeight: '500' }}>
+                            <div style={{ transform: 'translate(17.5px, 1px)' }}>
+                                <Check />
+                            </div>
+                            <div style={{ color: '#0070c9', transform: 'translate(34px, -27.8px)' }}>
+                                In stock
+                            </div>
+                            <div style={{ position: 'absolute', transform: 'translate(356px, -45.8px)', color: '#0070c9' }}>
+                                <Icon name="lock" />
+                                Secure Checkout
+                            </div>
+                        </div>
                     </div>
-                    <div style={{ padding: '5px 55px 20px 51px' }}>
-                        <b style={{ fontSize: '17px', fontWeight: '500' }}>
-                            <Icon name="check" size="large" /> In stock
-                        </b>
+                    <div style={{ transform: 'translateY(-50px)' }}>
+                        <Divider />
+                    </div>
+                    <div style={{ transform: 'translateY(-42px)' }}>
+                        <span style={{ fontSize: '24px', fontWeight: '600', padding: '0px 55px 58px 55px' }}>
+                            Total
+                        </span>
+                        <div style={{ fontSize: '24px', fontWeight: '600', padding: '0px 55px 58px 55px', transform: 'translate(413.5px, -24.5px)' }}>
+                            $817.60
+                        </div>
+                    </div>
+                    <div style={{ transform: 'translateY(-115px)' }}>
+                        <Divider />
                     </div>
                     <center>
                         <Button
@@ -43,17 +62,18 @@ export default function OneWifi() {
                                 color: 'white', 
                                 fontWeight: '100', 
                                 fontSize: '24px',
-                                padding: '15px 100px 15px 100px'
+                                padding: '15px 100px 15px 100px',
+                                transform: 'translateY(-85px)'
                             }}
                         >
                             <span>
-                                Buy with <Icon name="apple" />Pay
+                                Buy with <Icon name="apple" style={{ transform: 'scale(0.85) translateX(-6px)' }} /><div style={{ position: 'absolute', transform: 'translate(116px, -24.1px)' }}>Pay</div>
                             </span>
                         </Button>
                     </center>
                 </div>
             </div>
-            <div style={{ background: '#F5F5F7', width: '816px', height: '340px', transform: 'translate(0px, 524px)', borderRadius: '0px 0px 19.5px 19.5px', position: 'absolute' }} />
+            <div style={{ background: '#F5F5F7', width: '816px', height: '440px', transform: 'translate(0px, 524px)', borderRadius: '0px 0px 19.5px 19.5px', position: 'absolute' }} />
         </>
     );
 }
