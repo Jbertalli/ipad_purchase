0fbd06fe9b24d0384db7589ec02b236ab9d1704d
Jbertalli <jjbertalli@berkeley.edu>
2022-11-15 17:25:50 -0800

    mobile home center edit

--- components/buyipadMobile.js
+++ components/buyipadMobile.js
@@ -25,32 +25,44 @@ export default function BuyMobile () {
                 </Link>
             </div>
             {/* fixed is the key */}
-            <div style={{ transform: 'scale(4.5)', position: 'fixed', display: 'flex', justifyContent: 'center', zIndex: '100' }}>
-                <div style={{ transform: 'scale(0.1) translate(-60vw, 950px)' }}>
+            <div style={{ transform: 'scale(4.5)', position: 'fixed', zIndex: '100' }}>
+                <div style={{ transform: 'scale(0.1) translate(-70vw, 950px)', display: 'flex', justifyContent: 'center' }}>
                     <Flower />
                 </div>
-                <div style={{ transform: 'translate(56%, 16vh) scale(0.23)' }}>
-                    <Link href="/colorSelection/ipadhome">   
-                        <button 
-                            className={styles.button} 
-                            style={{ 
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', transform: 'translateY(540px)' }}>
+                <div style={{ background: 'black', width: '200px', height: '230px', color: 'white', fontFamily: 'Helvetica', position: 'absolute', zIndex: '2', transform: 'scale(0.8)' }}>
+                    <center>
+                        <div style={{ fontSize: '56px', fontWeight: '400', lineHeight: '44px' }}>
+                            iPad air
+                        </div>
+                        <Link href="/colorSelection/ipadhome">   
+                            <button className={styles.button} style={{ 
                                 borderRadius: '50px', 
                                 border: 'transparent', 
                                 padding: '8px 16px 8px 16px', 
                                 background: '#0066CC', 
                                 color: 'white', 
-                                marginBottom: '9px', 
-                                marginTop: '7px', 
+                                marginBottom: '19px', 
+                                marginTop: '67px', 
                                 fontWeight: '200px', 
                                 fontSize: '17px', 
-                                cursor: 'pointer' 
-                            }}
-                        >
-                            <a>
-                                Buy
-                            </a>
-                        </button>    
-                    </Link>
+                                cursor: 'pointer', 
+                                transform: 'scale(1.4)' }}>
+                                <a>
+                                    Buy
+                                </a>
+                            </button>    
+                        </Link> 
+                    </center>
+                    <div className={styles.wrapper2} style={{ transform: 'translate(87px, -193px) scale(0.5)', position: 'relative', zIndex: '0' }}>
+                        <div className={styles.neon_wrapper2}>
+                            <span className={styles.txt2} style={{ fontFamily: 'Brush Script MT, Brush Script Std, cursive', transform: 'translate(-10px, 0px)' }}>
+                                air
+                            </span>
+                            <span className={styles.gradient2} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
