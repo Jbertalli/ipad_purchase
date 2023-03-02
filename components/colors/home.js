0e8902e4c888318049064af3b6455d768f44571d
Jbertalli <jjbertalli@berkeley.edu>
2023-03-02 02:10:22 -0800

    misc edit...

--- components/colors/home.js
+++ components/colors/home.js
@@ -26,7 +26,7 @@ export default function Home() {
     <>
       <div
         style={{
-          transform: isPortrait ? 'translate(-50px)' : null
+          transform: isPortrait ? 'translate(-60px)' : null
         }}
       >
         <Grid
@@ -73,7 +73,7 @@ export default function Home() {
       </div>
       <div
         style={{
-          transform: isPortrait ? 'translate(-50px)' : null
+          transform: isPortrait ? 'translate(-60px)' : null
         }}
       >
         <Grid
