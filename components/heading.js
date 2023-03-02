ae0be667717270269bd1df8065abb145861c21da
Jbertalli <jjbertalli@berkeley.edu>
2023-03-02 03:13:36 -0800

    heading edit

--- components/heading.js
+++ components/heading.js
@@ -6,6 +6,8 @@ export default function Heading() {
   const isTablet = useMediaQuery(
     { minWidth: 100, maxWidth: 1290 }
   );
+
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
   
   return (
     <>
@@ -29,17 +31,23 @@ export default function Heading() {
             >
               iPad Air
             </span>
-            <span
+            <div
               style={{
-                fontSize: '17px',
-                fontWeight: '600',
-                display: 'flex',
-                justifyContent: isTablet ? 'flex-end' : 'center',
-                transform: isTablet ? 'translate(-100.5px, -7px)' : 'translate(344px, -7px)'
+                transform: isPortrait ? 'translate(50px)' : null
               }}
             >
-              From $599 or $49.91/mo. for 12 mo.*
-            </span>
+              <span
+                style={{
+                  fontSize: '17px',
+                  fontWeight: '600',
+                  display: 'flex',
+                  justifyContent: isTablet ? 'flex-end' : 'center',
+                  transform: isTablet ? 'translate(-100.5px, -7px)' : 'translate(344px, -7px)'
+                }}
+              >
+                From $599 or $49.91/mo. for 12 mo.*
+              </span>
+            </div>
             <Divider style={{ transform: 'translateY(-6.5px)' }} />
           </div>
         </div>
