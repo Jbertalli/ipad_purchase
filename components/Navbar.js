0746ce220bf870ccfd951fe3e4691ed74f95cb9f
Jbertalli <jjbertalli@berkeley.edu>
2023-03-03 03:45:43 -0800

    navbar edit3

--- components/Navbar.js
+++ components/Navbar.js
@@ -168,7 +168,7 @@ const Navbar = ({ user }) => {
                     style={{
                       color: '#F5F5F7',
                       margin: '0em .65em 0em .65em',
-                      transform: 'scale(1.2)'
+                      transform: 'scale(1.2)',
                     }}
                     active={isActive('/')}
                   >
@@ -239,268 +239,258 @@ const Navbar = ({ user }) => {
                 fontSize: '17px',
                 fontWeight: '400',
                 background: '#000000',
+                opacity: '0.8',
                 fontFamily: 'SF Pro Text',
                 width: '100vw'
               }}
             >
-              <div
+              <Container
                 style={{
+                  opacity: '0.8',
                   display: 'flex',
                   justifyContent: 'space-between',
-                  width: '100%'
                 }}
               >
-                {menuModal ? (
-                <>
+                <Menu.Item
+                  style={{ color: '#F5F5F7' }}
+                  onClick={() => setMenuModal(true)}
+                >
                   <div
-                    // className={styles.dropdown}
                     style={{
-                      width: '100%',
-                      height: '100vh',
-                      position: 'absolute'
+                      transform: 'scaleX(1.8) translate(0.3px, -11px)',
+                      color: '#F5F5F7',
                     }}
                   >
+                    _
+                  </div>
+                  <div
+                    style={{
+                      transform: 'scaleX(1.8) translate(-4.7px, -4px)',
+                      color: '#F5F5F7',
+                    }}
+                  >
+                    _
+                  </div>
+                </Menu.Item>
+                <Modal
+                  // className={styles.dropdown}
+                  open={menuModal}
+                  dimmer="blurring"
+                  size="small"
+                  style={{
+                    width: '816px',
+                    height: '570px',
+                    position: 'fixed',
+                    background: 'black',
+                    width: '100%',
+                    height: '100%',
+                    transform: 'translateY(-14px)',
+                  }}
+                >
+                  <div
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => setMenuModal(false)}
+                  >
                     <div
-                      style={{ cursor: 'pointer' }}
-                      onClick={() => setMenuModal(false)}
+                      style={{
+                        fontSize: '23px',
+                        fontWeight: '100',
+                        color: '#f5f5f7',
+                        transform: 'translate(18px, 10px) scaleX(1.3)',
+                        position: 'absolute',
+                        opacity: '0.6',
+                      }}
                     >
-                      <div
-                        style={{
-                          fontSize: '23px',
-                          fontWeight: '100',
-                          fontFamily: 'sans-serif',
-                          color: '#f5f5f7',
-                          transform: 'translate(18px, 10px) scaleX(1.3)',
-                          position: 'absolute',
-                          opacity: '0.6'
-                        }}
-                      >
-                        x
-                      </div>
-                      <div
-                        style={{
-                          display: 'flex',
-                          justifyContent: 'center',
-                          transform: 'translateY(-140px)'
-                        }}
-                      >
-                        <Logo
-                          style={{
-                            color: 'red',
-                            background: 'red',
-                            position: 'absolute'
-                          }}
-                        />
-                      </div>
+                      x
                     </div>
-                    <div 
-                      style={{ 
-                        transform: 'translateY(-274px)', 
-                        background: 'black' 
+                    <div
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        transform: 'translateY(-140px)',
+                      }}
+                    >
+                      <Logo
+                        style={{
+                          color: 'red',
+                          background: 'red',
+                          position: 'absolute',
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div style={{ transform: 'translateY(-274px)' }}>
+                    <div
+                      style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        position: 'relative',
+                        marginBottom: '51px',
                       }}
                     >
                       <div
                         style={{
-                          display: 'flex',
-                          justifyContent: 'center',
-                          position: 'relative',
-                          marginBottom: '51px',
-                          transform: 'translateY(7px)'
+                          background: 'gray',
+                          fontWeight: '400',
+                          height: '36px',
+                          width: '69.5vw',
+                          position: 'absolute',
+                          borderRadius: '6px',
+                          opacity: '0.2',
                         }}
-                      >
-                        <div
-                          style={{
-                            background: 'gray',
-                            fontWeight: '400',
-                            height: '36px',
-                            width: '69.5vw',
-                            position: 'absolute',
-                            borderRadius: '6px',
-                            opacity: '0.2'
-                          }}
-                        />
-                        <div
-                          style={{
-                            color: '#86868b',
-                            fontSize: '17px',
-                            position: 'fixed',
-                            transform: 'translate(0%, 40%)',
-                            opacity: '0.8',
-                            left: '16.5%'
-                          }}
-                        >
-                          <Icon name="search" />
-                          Search apple.com
-                        </div>
-                      </div>
-                      <Divider
-                        style={{ background: '#f5f5f7', opacity: '0.3' }}
                       />
                       <div
                         style={{
-                          color: '#f5f5f7',
-                          fontWeight: '100',
-                          lineHeight: '14.6px',
+                          color: '#86868b',
                           fontSize: '17px',
-                          width: '100%',
-                          cursor: 'pointer',
-                          padding: '0px 23px 0px 23px',
-                          background: 'black'
+                          position: 'fixed',
+                          transform: 'translate(0%, 50%)',
+                          opacity: '0.8',
+                          left: '16.5%',
                         }}
                       >
-                        <Link href="/" passHref>
-                          <div>Store</div>
-                        </Link>
-                        <Divider
-                          style={{ background: '#f5f5f7', opacity: '0.3' }}
-                        />
-                        <Link href="/" passHref>
-                          <div>Mac</div>
-                        </Link>
-                        <Divider
-                          style={{ background: '#f5f5f7', opacity: '0.3' }}
-                        />
-                        <Link href="/" passHref>
-                          <div>iPad</div>
-                        </Link>
-                        <Divider
-                          style={{ background: '#f5f5f7', opacity: '0.3' }}
-                        />
-                        <Link href="/" passHref>
-                          <div>iPhone</div>
-                        </Link>
-                        <Divider
-                          style={{ background: '#f5f5f7', opacity: '0.3' }}
-                        />
-                        <Link href="/" passHref>
-                          <div>Watch</div>
-                        </Link>
-                        <Divider
-                          style={{ background: '#f5f5f7', opacity: '0.3' }}
-                        />
-                        <Link href="/" passHref>
-                          <div>AirPods</div>
-                        </Link>
-                        <Divider
-                          style={{ background: '#f5f5f7', opacity: '0.3' }}
-                        />
-                        <Link href="/" passHref>
-                          <div>TV & Home</div>
-                        </Link>
-                        <Divider
-                          style={{ background: '#f5f5f7', opacity: '0.3' }}
-                        />
-                        <Link href="/" passHref>
-                          <div>Only on Apple</div>
-                        </Link>
-                        <Divider
-                          style={{ background: '#f5f5f7', opacity: '0.3' }}
-                        />
-                        <Link href="/" passHref>
-                          <div>Accessories</div>
-                        </Link>
-                        <Divider
-                          style={{ background: '#f5f5f7', opacity: '0.3' }}
-                        />
-                        <Link href="/" passHref>
-                          <div>Support</div>
-                        </Link>
-                        <Divider
-                          style={{ background: '#f5f5f7', opacity: '0.3' }}
-                        />
-                        <Menu.Item>
-                          <Dropdown
-                            text="Account"
-                            simple
-                            style={{ 
-                              color: 'white', 
-                              transform: 'translate(-19.5px, -16.3px)'
-                            }}
-                          >
-                            <Dropdown.Menu>
-                              {user ? (
-                                <>
-                                  <Dropdown.Item>
-                                    <Link href="/account" passHref>
-                                      <strong>{user.name}</strong>
-                                    </Link>
-                                  </Dropdown.Item>
-                                  <Dropdown.Item onClick={handleLogout}>
-                                    <strong>Logout</strong>
-                                  </Dropdown.Item>
-                                </>
-                              ) : (
-                                <>
-                                  <Dropdown.Item
-                                    href="/signup"
-                                    active={isActive('/signup')}
-                                  >
-                                    <strong>Signup</strong>
-                                  </Dropdown.Item>
-                                  <Dropdown.Divider />
-                                  <Dropdown.Item
-                                    href="/login"
-                                    active={isActive('/login')}
-                                  >
-                                    <strong>Login</strong>
-                                  </Dropdown.Item>
-                                </>
-                              )}
-                            </Dropdown.Menu>
-                          </Dropdown>
-                        </Menu.Item>
+                        <Icon name="search" />
+                        Search apple.com
                       </div>
                     </div>
+                    <Divider
+                      style={{ background: '#f5f5f7', opacity: '0.3' }}
+                    />
+                    <Container
+                      style={{
+                        color: '#f5f5f7',
+                        fontWeight: '100',
+                        lineHeight: '14.6px',
+                        fontSize: '17px',
+                        width: '584px',
+                        cursor: 'pointer',
+                        padding: '0px 23px 0px 23px',
+                      }}
+                    >
+                      <Link href="/" passHref>
+                        <div>Store</div>
+                      </Link>
+                      <Divider
+                        style={{ background: '#f5f5f7', opacity: '0.3' }}
+                      />
+                      <Link href="/" passHref>
+                        <div>Mac</div>
+                      </Link>
+                      <Divider
+                        style={{ background: '#f5f5f7', opacity: '0.3' }}
+                      />
+                      <Link href="/" passHref>
+                        <div>iPad</div>
+                      </Link>
+                      <Divider
+                        style={{ background: '#f5f5f7', opacity: '0.3' }}
+                      />
+                      <Link href="/" passHref>
+                        <div>iPhone</div>
+                      </Link>
+                      <Divider
+                        style={{ background: '#f5f5f7', opacity: '0.3' }}
+                      />
+                      <Link href="/" passHref>
+                        <div>Watch</div>
+                      </Link>
+                      <Divider
+                        style={{ background: '#f5f5f7', opacity: '0.3' }}
+                      />
+                      <Link href="/" passHref>
+                        <div>AirPods</div>
+                      </Link>
+                      <Divider
+                        style={{ background: '#f5f5f7', opacity: '0.3' }}
+                      />
+                      <Link href="/" passHref>
+                        <div>TV & Home</div>
+                      </Link>
+                      <Divider
+                        style={{ background: '#f5f5f7', opacity: '0.3' }}
+                      />
+                      <Link href="/" passHref>
+                        <div>Only on Apple</div>
+                      </Link>
+                      <Divider
+                        style={{ background: '#f5f5f7', opacity: '0.3' }}
+                      />
+                      <Link href="/" passHref>
+                        <div>Accessories</div>
+                      </Link>
+                      <Divider
+                        style={{ background: '#f5f5f7', opacity: '0.3' }}
+                      />
+                      <Link href="/" passHref>
+                        <div>Support</div>
+                      </Link>
+                      <Divider
+                        style={{ background: '#f5f5f7', opacity: '0.3' }}
+                      />
+                      <Menu.Item>
+                        <Dropdown
+                          text="Account"
+                          simple
+                          style={{ color: 'white' }}
+                        >
+                          <Dropdown.Menu>
+                            {user ? (
+                              <>
+                                <Dropdown.Item>
+                                  <Link href="/account" passHref>
+                                    <strong>{user.name}</strong>
+                                  </Link>
+                                </Dropdown.Item>
+                                <Dropdown.Item onClick={handleLogout}>
+                                  <strong>Logout</strong>
+                                </Dropdown.Item>
+                              </>
+                            ) : (
+                              <>
+                                <Dropdown.Item
+                                  href="/signup"
+                                  active={isActive('/signup')}
+                                >
+                                  <strong>Signup</strong>
+                                </Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item
+                                  href="/login"
+                                  active={isActive('/login')}
+                                >
+                                  <strong>Login</strong>
+                                </Dropdown.Item>
+                              </>
+                            )}
+                          </Dropdown.Menu>
+                        </Dropdown>
+                      </Menu.Item>
+                    </Container>
                   </div>
-                </>
-                ):(
-                <>
-                  <Menu.Item
-                    style={{ color: '#F5F5F7' }}
-                    onClick={() => setMenuModal(true)}
-                  >
+                </Modal>
+                <Link href="/" passHref>
+                  <Menu.Item>
                     <div
                       style={{
-                        transform: 'scaleX(1.8) translate(0.3px, -11px)',
-                        color: '#F5F5F7',
+                        cursor: 'pointer',
+                        transform: 'scale(1.1) translateY(-3px)',
                       }}
                     >
-                      _
-                    </div>
-                    <div
-                      style={{
-                        transform: 'scaleX(1.8) translate(-4.7px, -4px)',
-                        color: '#F5F5F7',
-                      }}
-                    >
-                      _
+                      <Logo />
                     </div>
                   </Menu.Item>
-                  <Link href="/" passHref>
-                    <Menu.Item>
-                      <div
-                        style={{
-                          cursor: 'pointer',
-                          transform: 'scale(1.1) translateY(-3px)'
-                        }}
-                      >
-                        <Logo />
-                      </div>
-                    </Menu.Item>
-                  </Link>
-                  <Link href="/" passHref>
-                    <Menu.Item
-                      style={{
-                        color: '#F5F5F7',
-                        transform: 'scale(1.1) translateY(-1.5px)'
-                      }}
-                    >
-                      <Icon name="suitcase" />
-                    </Menu.Item>
-                  </Link>
-                </>
-                )}
-              </div>
+                </Link>
+                <Link href="/" passHref>
+                  <Menu.Item
+                    style={{
+                      color: '#F5F5F7',
+                      transform: 'scale(1.1) translateY(-1.5px)',
+                    }}
+                  >
+                    <Icon name="suitcase" />
+                  </Menu.Item>
+                </Link>
+              </Container>
             </Menu>
           </Sticky>
         </>
